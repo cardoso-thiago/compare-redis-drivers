d098ddaab5f9815a32d76fa99819a72f262c93fb
cardoso-thiago <thiago.cardoso33@gmail.com>
2021-06-03 00:41:31 -0300

    Testes com ssl

--- demo-lettuce/src/main/kotlin/br/com/cardoso/redisson/DemoLettuceApplication.kt
+++ demo-lettuce/src/main/kotlin/br/com/cardoso/redisson/DemoLettuceApplication.kt
@@ -3,11 +3,30 @@ package br.com.cardoso.redisson
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import java.time.Duration
 
 @SpringBootApplication
 @EnableCaching
-class DemoLettuceApplication
+class DemoLettuceApplication {
+
+    @Bean
+    fun lettuceConnectionFactory(): LettuceConnectionFactory {
+        val clientConfig: LettuceClientConfiguration = LettuceClientConfiguration.builder()
+            .useSsl().and()
+            .commandTimeout(Duration.ofSeconds(2))
+            .shutdownTimeout(Duration.ZERO)
+            .build()
+        return LettuceConnectionFactory(
+            RedisStandaloneConfiguration("localhost", 6379),
+            clientConfig
+        )
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<DemoLettuceApplication>(*args)
+    runApplication<DemoLettuceApplication>(*args)
 }
